5bae321b2cde7fbd71ed93211eb910376d1a78e1
bbonche <bbonche@henix.fr>
2022-10-18 11:08:20 +0200

    add Redis config

---
+++ src/main/java/fr/ans/psc/context/sharing/api/config/RedisConfiguration.java
@@ -0,0 +1,21 @@
+package fr.ans.psc.context.sharing.api.config;
+
+import fr.ans.psc.context.sharing.api.model.PscContext;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, PscContext> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, PscContext> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
