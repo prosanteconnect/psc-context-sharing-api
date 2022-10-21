00ab37331d7411a7cf984e0c0c4d41007cec5597
bbonche <bbonche@henix.fr>
2022-10-21 16:11:57 +0200

    add properties files

--- src/test/java/fr/ans/psc/context/sharing/api/repository/PsContextRepositoryTest.java
+++ src/test/java/fr/ans/psc/context/sharing/api/repository/PsContextRepositoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.data.redis.AutoConfigureDataRedis;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
