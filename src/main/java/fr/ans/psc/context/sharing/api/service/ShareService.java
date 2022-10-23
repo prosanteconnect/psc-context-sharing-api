175356aabf23a7d90f1f67babed24ad767f319a9
bbonche <bbonche@henix.fr>
2022-10-23 18:47:17 +0200

    handle PscContext Not found case

--- src/main/java/fr/ans/psc/context/sharing/api/service/ShareService.java
+++ src/main/java/fr/ans/psc/context/sharing/api/service/ShareService.java
@@ -13,8 +13,10 @@ import fr.ans.psc.context.sharing.api.model.PscContext;
 import fr.ans.psc.context.sharing.api.repository.PscContextRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -30,9 +32,11 @@ public class ShareService {
         validateSchemaConformity(pscContext);
         PscContext saved;
         try {
+            log.debug("Trying to save entry for key {} in Redis server...", pscContext.getPsId());
             saved = pscContextRepository.save(pscContext);
+            log.debug("Entry for key {} successfully saved", pscContext.getPsId());
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Error occurred while requesting Redis server");
             throw new PscCacheException();
         }
         return saved;
@@ -41,8 +45,16 @@ public class ShareService {
     public PscContext getPscContext(String nationalId) throws PscCacheException {
         PscContext pscContext;
         try {
-            pscContext = pscContextRepository.findById(nationalId).get();
+            log.debug("requesting Redis server for key {}...", nationalId);
+            Optional<PscContext> optional = pscContextRepository.findById(nationalId);
+            if (optional.isPresent()) {
+                pscContext = optional.get();
+            } else {
+                log.debug("No entry for key {}", nationalId);
+                throw new PscCacheException(HttpStatus.NOT_FOUND);
+            }
         } catch (Exception e) {
+            log.error("Error occurred while requesting Redis server");
             throw new PscCacheException();
         }
         return pscContext;
